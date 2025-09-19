05d7203076ead49134192350be92974d990ed3a5
Tate Costa <tatecosta2022@gmail.com>
2025-09-19 22:49:28 +0000

    Okay I want the users to be Sam Phillips, Logan Myers, Ryan Van, Connor

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,24 @@
 import type { User } from './types';
 
 export const USERS: User[] = [
-  { id: 'user1', name: 'Simone Biles' },
-  { id: 'user2', name: 'Kohei Uchimura' },
-  { id: 'user3', name: 'Nadia Comăneci' },
+  { id: 'user1', name: 'Sam Phillips' },
+  { id: 'user2', name: 'Logan Myers' },
+  { id: 'user3', name: 'Ryan Van' },
+  { id: 'user4', name: 'Connor Micklos' },
+  { id: 'user5', name: 'Tate Costa' },
+  { id: 'user6', name: 'Sam Kaplan' },
+  { id: 'user7', name: 'Preston Ngai' },
+  { id: 'user8', name: 'Vahe Petrosyan' },
+  { id: 'user9', name: 'Dylan Sheppard' },
+  { id: 'user10', name: 'Garret Schooley' },
+  { id: 'user11', name: 'Brandon Dang' },
+  { id: 'user12', name: 'Nathan Whitaker' },
+  { id: 'user13', name: 'Ian Sandoval' },
+  { id: 'user14', name: 'Jax Washburn' },
+  { id: 'user15', name: 'Matthew Nguyen' },
+  { id: 'user16', name: 'Hasan Aydogdu' },
+  { id: 'user17', name: 'Alex Karadzov' },
+  { id: 'user18', name: 'Gavin Doherty' },
 ];
 
 export const EVENTS = ['FX', 'PH', 'SR', 'VT', 'PB', 'HB'];
